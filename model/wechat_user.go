be83e2d12e544f2d6f0b40ea0705ee65c54df01a
godcong <jumbycc@163.com>
2019-02-19 01:30:03 +0800

    update spread

--- model/wechat_user.go
+++ model/wechat_user.go
@@ -1,6 +1,9 @@
 package model
 
-import "github.com/godcong/wego/core"
+import (
+	"github.com/godcong/wego-spread-service/util"
+	"github.com/godcong/wego/core"
+)
 
 // RegTypeH5 ...
 const RegTypeH5 = "0"
@@ -14,18 +17,17 @@ type WechatUser struct {
 	Block                bool   `xorm:"notnull default(false) comment(禁止访问)"`                      //禁止访问
 	AppID                string `xorm:"notnull default('') comment(appid)" json:"appid,omitempty"` //appid
 	Type                 int    `xorm:"notnull default(0) comment(微信or小程序用户标识)" json:",omitempty"` //type
-	Sign                 string `xorm:"notnull default('')`
+	Sign                 string `xorm:"notnull unique default('')  comment(分享码)"`
 	*core.WechatUserInfo `xorm:"extends" json:",inline"`
 }
 
 // UserFromHook ...
-func UserFromHook(info core.WechatUserInfo, id string, typ int) *WechatUser {
+func UserFromHook(info *core.WechatUserInfo, id string, typ int) *WechatUser {
 	return &WechatUser{
-		Model:          Model{},
 		Block:          false,
 		AppID:          id,
 		Type:           typ,
-		Sign:           ,
-		WechatUserInfo: &info,
+		Sign:           util.GenSpreadSign(),
+		WechatUserInfo: info,
 	}
 }
