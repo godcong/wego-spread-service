c49c391166b08bbc1a3511d81b7b664d14abca9e
godcong <jumbycc@163.com>
2019-02-19 02:53:18 +0800

    update spread

--- controller/wechat_authorize_controller.go
+++ controller/wechat_authorize_controller.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/go-xorm/xorm"
 	"github.com/godcong/wego"
 	"github.com/godcong/wego-spread-service/cache"
 	"github.com/godcong/wego-spread-service/model"
@@ -57,41 +58,66 @@ func TokenHook(userSign *string) wego.TokenHook {
 // UserHook ...
 func UserHook(userSign string, id string, t int) wego.UserHook {
 	return func(user *core.WechatUserInfo) []byte {
-		wu := model.UserFromHook(user, id, t)
-		if wu == nil {
-			log.Error(xerrors.New("null wechat user"))
-			return nil
-		}
-		i, e := model.Insert(nil, wu)
-		if e != nil || i == 0 {
-			log.Error(e, i)
-			return nil
-		}
-		parent := &model.Spread{
-			SelfSign: userSign,
-		}
-		b, e := model.Get(nil, parent)
-		if e != nil || !b {
-			log.Error(e, b)
-			//continue
-		}
-		spread := &model.Spread{
-			WechatUserID: wu.ID,
-			SelfSign:     wu.Sign,
-			ParentSign:   userSign,
-			ParentSign2:  parent.ParentSign,
-			ParentSign3:  parent.ParentSign2,
-			ParentSign4:  parent.ParentSign3,
-			ParentSign5:  parent.ParentSign4,
-			ParentSign6:  parent.ParentSign5,
-			ParentSign7:  parent.ParentSign6,
-			ParentSign8:  parent.ParentSign7,
-			ParentSign9:  parent.ParentSign8,
-		}
-		i, e = model.Insert(nil, spread)
-		if e != nil || i == 0 {
-			log.Error(e, i)
-			return nil
+		_, e := model.DB().Transaction(func(session *xorm.Session) (v interface{}, e error) {
+			wu := model.UserFromHook(user, id, t)
+			defer func() {
+				if e != nil {
+					log.Error(e)
+					e = session.Rollback()
+				}
+			}()
+			if wu == nil {
+				return nil, xerrors.New("null wechat user")
+			}
+			i, e := model.Insert(nil, wu)
+			if e != nil || i == 0 {
+				log.Error("wechat user insert:", i)
+				return nil, xerrors.New("wechat user insert error")
+			}
+
+			user := &model.User{
+				WechatUserID: wu.ID,
+				Enable:       false,
+				Nickname:     wu.Nickname,
+				Sign:         util.GenMD5(uuid.New().String()),
+				Salt:         util.GenerateRandomString(16),
+			}
+			i, e = model.Insert(nil, user)
+			if e != nil || i == 0 {
+				log.Error("user insert:", i)
+				return nil, xerrors.New("user insert error")
+			}
+
+			parent := &model.Spread{
+				SelfSign: userSign,
+			}
+			b, e := model.Get(nil, parent)
+			if e != nil || !b {
+				log.Error("parent:", b)
+				//continue
+			}
+			spread := &model.Spread{
+				WechatUserID: wu.ID,
+				SelfSign:     user.Sign,
+				ParentSign:   userSign,
+				ParentSign2:  parent.ParentSign,
+				ParentSign3:  parent.ParentSign2,
+				ParentSign4:  parent.ParentSign3,
+				ParentSign5:  parent.ParentSign4,
+				ParentSign6:  parent.ParentSign5,
+				ParentSign7:  parent.ParentSign6,
+				ParentSign8:  parent.ParentSign7,
+				ParentSign9:  parent.ParentSign8,
+			}
+			i, e = model.Insert(nil, spread)
+			if e != nil || i == 0 {
+				log.Error("spread insert", i)
+				return nil, e
+			}
+			return nil, nil
+		})
+		if e != nil {
+			log.Error(e)
 		}
 		return nil
 	}
