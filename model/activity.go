d187955377529eeb49689d5d85f2c3b9d3c64039
godcong <jumbycc@163.com>
2019-02-20 01:05:06 +0800

    update spread

--- model/activity.go
+++ model/activity.go
@@ -2,6 +2,9 @@ package model
 
 // Activity ...
 type Activity struct {
-	Model      `xorm:"extends" json:",inline"`
-	SpreadMark string `xorm:"notnull unique default('') spread_mark" json:"spread_mark"`
+	Model        `xorm:"extends" json:",inline"`
+	UserID       string `xorm:"notnull unique default('') user_id" json:"user_id"`
+	PropertyID   string `xorm:"notnull unique default('') property_id" json:"property_id"`
+	ActivityCode string `xorm:"notnull unique default('') activity_code" json:"activity_code"` //活动码唯一ID
+	Mode         string `xorm:"notnull default('')" json:"mode"`                               //活动模式
 }
