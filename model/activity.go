bb343773e7dc211d687eae6d69b39719f21f6e4a
godcong <jumbycc@163.com>
2019-02-20 01:13:05 +0800

    update spread

--- model/activity.go
+++ model/activity.go
@@ -3,9 +3,16 @@ package model
 // Activity ...
 type Activity struct {
 	Model        `xorm:"extends" json:",inline"`
-	UserID       string `xorm:"notnull unique default('') user_id" json:"user_id"`
-	PropertyID   string `xorm:"notnull unique default('') property_id" json:"property_id"`
-	ActivityCode string `xorm:"notnull unique default('') activity_code" json:"activity_code"` //活动码唯一ID
-	Verify       bool   `xorm:"notnull default(false)" json:"verify"`                          //是否校验
-	Mode         string `xorm:"notnull default('')" json:"mode"`                               //活动模式
+	UserID       string `xorm:"notnull unique default('') comment(用户ID) user_id" json:"user_id"`
+	PropertyID   string `xorm:"notnull unique default('') comment(配置ID) property_id" json:"property_id"`
+	ActivityCode string `xorm:"notnull unique default('') comment(活动码唯一ID) activity_code" json:"activity_code"` //活动码唯一ID
+	Verify       bool   `xorm:"notnull default(false) comment(是否校验) verify" json:"verify"`                      //是否校验
+	Mode         string `xorm:"notnull default('') comment(活动模式) mode" json:"mode"`                             //活动模式
+}
+
+// NewActivity ...
+func NewActivity(id string) *Activity {
+	return &Activity{
+		Model: Model{ID: id},
+	}
 }
