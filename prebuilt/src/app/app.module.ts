d03a7e4671b520cca9640d781d4686c1e27e7fc4
godcong <jumbycc@163.com>
2019-03-01 01:39:45 +0800

    update wego-spread-service

--- prebuilt/src/app/app.module.ts
+++ prebuilt/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
