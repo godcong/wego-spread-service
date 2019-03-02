6728925266548674a257266197c4f335b1dc2c20
godcong <jumbycc@163.com>
2019-03-03 01:39:22 +0800

    update wego-spread-service

--- prebuilt/src/app/app-routing.module.ts
+++ prebuilt/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule, ExtraOptions} from '@angular/router';
+import {QrcodeComponent} from './qrcode/qrcode.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'code', component: QrcodeComponent,
+  },
+  {path: '', redirectTo: '/code', pathMatch: 'full'},
+];
+
+const config: ExtraOptions = {
+  useHash: false,
+  enableTracing: true,
+};
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, config)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
