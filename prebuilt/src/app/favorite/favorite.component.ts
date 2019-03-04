742bda80ed6bdede7d1642d319b9994598f66ffd
godcong <jumbycc@163.com>
2019-03-04 23:53:14 +0800

    update wego-spread-service

--- prebuilt/src/app/favorite/favorite.component.ts
+++ prebuilt/src/app/favorite/favorite.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var window: Window;
 @Component({
   selector: 'app-favorite',
   templateUrl: './favorite.component.html',
   styleUrls: ['./favorite.component.scss']
 })
 export class FavoriteComponent implements OnInit {
+  private height: number;
 
-  constructor() { }
+  constructor() {
+    this.height = window.innerHeight;
+  }
 
   ngOnInit() {
   }
