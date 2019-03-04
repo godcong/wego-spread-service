95e6728c12de2512d4383220b74d3aaed782a1ea
godcong <jumbycc@163.com>
2019-03-04 22:08:03 +0800

    update wego-spread-service

--- prebuilt/src/app/home/home.component.ts
+++ prebuilt/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {HomeDataService} from './home-data.service';
 
 @Component({
@@ -6,18 +6,23 @@ import {HomeDataService} from './home-data.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnChanges {
   private links: string[] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '9', '9', '9', '9', '9', '9', '9', '9', '9', '9'];
   private data: HomeDataService;
   public activities: any;
 
   constructor(data: HomeDataService) {
     this.data = data;
-  }
-
-  ngOnInit() {
     this.activities = this.data.getActivityList();
   }
 
+  ngOnInit() {
+    // this.activities = this.data.getActivityList();
+  }
+
+
+  ngOnChanges(changes: SimpleChanges): void {
+    // this.activities = this.data.getActivityList();
+  }
 
 }
