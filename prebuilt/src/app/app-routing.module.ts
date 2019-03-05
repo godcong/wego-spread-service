7afbb17b3e6f04cb9ab993c4fcb529bbd004f1d4
godcong <jumbycc@163.com>
2019-03-06 02:38:20 +0800

    update wego-spread-service

--- prebuilt/src/app/app-routing.module.ts
+++ prebuilt/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule, ExtraOptions} from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {MyComponent} from './my/my.component';
+import {FavoriteComponent} from './favorite/favorite.component';
 
 const routes: Routes = [
   {
     path: 'home', component: HomeComponent,
+  },
+  {
+    path: 'my', component: MyComponent,
+  },
+  {
+    path: 'favorite', component: FavoriteComponent,
   },
   {
     path: '', redirectTo: '/home', pathMatch: 'full'
