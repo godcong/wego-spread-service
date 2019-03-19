d0416d84eb5fa0393833bb1b9facbd49ac23b589
godcong <jumbycc@163.com>
2019-03-20 02:12:28 +0800

    update wego-spread-service

--- prebuilt/src/app/share-code/share-code.component.ts
+++ prebuilt/src/app/share-code/share-code.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SizeService} from '../size.service';
+import {ActivatedRoute, ParamMap} from '@angular/router';
+import {DataService} from '../data.service';
+import {WechatService} from '../wechat.service';
 
 @Component({
   selector: 'app-share-code',
@@ -6,10 +10,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./share-code.component.scss']
 })
 export class ShareCodeComponent implements OnInit {
+  public size: SizeService;
+  public activityCode: string;
+  private router: ActivatedRoute;
+  private id: string;
+  private user: string;
+  private data: DataService;
+  private wechat: WechatService;
 
-  constructor() { }
+  constructor(size: SizeService, router: ActivatedRoute, data: DataService, wechat: WechatService) {
+    this.size = size;
+    this.router = router;
+    this.data = data;
+    this.wechat = wechat;
+    this.activityCode = 'http://localhost:8080';
+  }
 
   ngOnInit() {
+    this.router.queryParamMap.subscribe((params: ParamMap) => {
+      console.log(params);
+      this.user = params.get('user');
+    });
+    this.router.paramMap.subscribe((params: ParamMap) => {
+      console.log(params);
+      this.id = params.get('id');
+    });
+    this.data.getSpreadShareInfo(this.id, this.user).subscribe((params: any) => {
+      console.log(params);
+      this.activityCode = params.url;
+      this.wechat.init(params.config);
+    });
+  }
+
+  onSuccess() {
+    console.log(this.activityCode);
   }
 
 }
